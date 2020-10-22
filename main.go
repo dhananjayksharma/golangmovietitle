66278a83a33fb7d779f93f0a9d707b32ea0b5de6
Dhananjay Sharma <dhananjayksharma@gmail.com>
2020-10-22 20:57:00 +0530

    demo

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/gin-gonic/gin"
+	"rainerdemotfs-westeu.visualstudio.com/golang-build/mathalgorithms"
+)
+
+func main() {
+	var port = flag.String("port", ":8080", "Port to use")
+
+	flag.Parse()
+
+	r := gin.Default()
+	r.GET("/fib", func(c *gin.Context) {
+		fibResult := []int32{}
+		fib := mathalgorithms.NewFibonacciIterator()
+		for fib.Next() {
+			fibResult = append(fibResult, fib.Value())
+			if fib.Value() > 80 {
+				break
+			}
+		}
+
+		c.JSON(200, fibResult)
+	})
+	r.Run(*port)
+}
